9a6bb95899ebc47387a7ddda7eed3003e422cd51
Денис <denisboarsci@gmail.com>
2024-11-21 13:41:45 +0500

    done 21.11.24 Правки для красоты и читаемости

--- module_2_2.py
+++ module_2_2.py
@@ -2,8 +2,8 @@ first = int(input("Первое число: "))
 second = int(input("Второе число: "))
 third = int(input("Третее число: "))
 if first == second == third:
-    print(3)
+    print('Числа равны:',3)
 elif first == second or first == third or second == third:
-    print(2)
+    print('Только 2 числа равны:',2)
 else:
-    print(0)
+    print('Никакие из чисел не равны:',0)
