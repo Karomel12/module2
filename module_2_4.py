51abe37a33ee13e31104939f5a8231ed820b2704
Денис <denisboarsci@gmail.com>
2024-11-26 07:30:46 +0500

    done 26.11.24

--- module_2_4.py
+++ module_2_4.py
@@ -1,16 +1,23 @@
 numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15]
 primes = []
 not_primes = []
-is_prime = True
 for i in numbers:
+    if i == 1:
+        continue
     for n in numbers:
-        if i % n == 0 and i > n:
-            primes.append(i)                            # 3 / 1, 3 / 2
+        is_prime = True
+        if n == i or n == 1:
+            if n == 1:
+                continue
             continue
-        else:
+        if i % n == 0:
+            is_prime = False
             not_primes.append(i)
             break
-    numbers.pop(0)
-
+    else:
+        if is_prime:
+            primes.append(i)
+        else:
+            not_primes.append(i)
 print('Четные:', primes)
 print('Не четные:', not_primes)
